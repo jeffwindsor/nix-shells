8aeb96c24ec4de9382dd11c3e9a589657a4cb592
Windsor, Jeff <jwindsor@cj.com>
2025-03-18 14:20:25 -0700

    AWS added cdk

--- k8s.nix
+++ k8s.nix
@@ -1,12 +1,13 @@
 { pkgs ? import <nixpkgs> {} }:
 pkgs.mkShell {
-	name = "AWS";
-	packages = with pkgs; [ awscli2 nodePackages.aws-cdk ];
+	name = "k8s";
+	packages = with pkgs; [ k9s kustomize kubectl ];
   shellHook = ''
     echo -e "\e[1;33mDevelopment Environment\e[0;32m"
     # add --version or some other call to list dev packages
-    echo -e "https://operations.cj.dev/"
-    aws --version
+    k9s version
+		kustomize version
+		kubectl version
     echo -e "\e[0m"
     '';
 }
