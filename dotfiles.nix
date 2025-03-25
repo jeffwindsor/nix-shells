bb2526a213cb7f4bedc35acacb0f312ef3e1abc3
Jeff Windsor <jeff.windsor@gmail.com>
2025-03-24 22:32:10 -0700

    Added shell output to dotfiles

--- dotfiles.nix
+++ dotfiles.nix
@@ -1,8 +1,7 @@
 { pkgs ? import <nixpkgs> {} }:
 pkgs.mkShell {
 	
-    # name (default: nix-shell). Set the name of the derivation.
-	# name = "";
+	name = "dotfiles";
   	
     # packages (default: []). Add executable packages to the nix-shell environment.
 	packages = with pkgs; [
@@ -21,13 +20,13 @@ pkgs.mkShell {
     yaml-language-server
 	];
 
-  # inputsFrom (default: []). Add build dependencies of the listed derivations to the nix-shell environment.
-	# inputsFrom = [ pkgs.hello ];
-
-    # shellHook (default: ""). Bash statements that are executed by nix-shell.
-  	shellHook = ''
-  	echo -e "\e[1;33mDevelopment Environment\e[0;32m"
-  	# add --version or some other call to list dev packages
+  # shellHook (default: ""). Bash statements that are executed by nix-shell.
+  shellHook = ''
+  	echo -e "\e[1;33mDevelopment Environment for dotfiles\e[0;32m"
+  	nixd --version
+    taplo --version
+    # shellcheck --version
+    echo -e "bash-language-server $(bash-language-server --version)"
 		echo -e "\e[0m"
-  	'';
+  '';
 }
